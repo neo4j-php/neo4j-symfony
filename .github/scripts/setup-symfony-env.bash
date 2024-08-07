21d568d71e61d07dc10cfbbaf34ffe0a029d7022
exaby73 <nabeelparkar99@gmail.com>
2024-08-07 12:14:17 +0530

    feat: Add Symfony override script to CI

--- .github/scripts/setup-symfony-env.bash
+++ .github/scripts/setup-symfony-env.bash
@@ -2,8 +2,34 @@
 
 set -ex
 
-cp composer.json composer.json.origin
-rm composer.lock || true
-rm -Rf vendor || true
-sed -i 's/\^5.4 || \^6.0 || \^7.0/\^5.4/g' composer.json
-composer install
+if [ -z "$1" ]; then
+    echo "Please specify the Symfony version to install"
+    exit 1
+fi
+
+echo "Installing Symfony version $1"
+
+# This is not required for CI, but it allows to test the script locally
+function cleanup {
+    echo "Cleaning up"
+    # Restore the original composer.json file
+    mv composer.origin.json composer.json
+}
+
+function install-specified-symfony-version {
+    local symfony_version=$1
+    # Save the original composer.json file
+    cp composer.json composer.origin.json
+    # Delete the lock file and vendor directory for a clean install
+    rm composer.lock || true
+    rm -Rf vendor || true
+    # Replace the Symfony version in composer.json
+    sed -i 's/\^5.4 || \^6.0 || \^7.0/'$symfony_version'/g' composer.json
+    # Install the specified Symfony version
+    composer install
+}
+
+# Ensure cleanup is called on exit
+trap cleanup EXIT
+
+install-specified-symfony-version $1
